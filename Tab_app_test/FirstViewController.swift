ee27f54201f1602ac40a9c85c9b4c057ba76bc54
masato <masato@m-mbp.local>
2018-11-06 14:41:09 +0800

    Modal Screen Transaction Programatically

--- Tab_app_test/FirstViewController.swift
+++ Tab_app_test/FirstViewController.swift
@@ -13,8 +13,37 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .green
+        let label_1 = UILabel(frame: view.frame)
+        label_1.text = "Screen 1"
+        view.addSubview(label_1)
+
+        // set Button by func
+        transScreenButton()
     }
 
+    // make button
+    func transScreenButton()  {
+        let button_1 = UIButton(frame: CGRect(x: self.view.frame.width / 2 - 200 / 2, y: self.view.frame.height / 2 - 150 / 2, width: 200, height: 150))
+        button_1.titleLabel?.text = "Screen Transition Button"
+        button_1.backgroundColor = .blue
+        button_1.addTarget(self, action: #selector(transScreen), for: .touchUpInside)
+
+        view.addSubview(button_1)
+
+    }
+
+
+    // set Screen Transaction Action
+
+    @objc func transScreen() {
+        let secondScreen = SecondViewController()
+
+        secondScreen.modalTransitionStyle = .coverVertical
+        present(secondScreen, animated: true, completion: nil)
+
+    }
 
 }
 
--- Tab_app_test/SecondViewController.swift
+++ Tab_app_test/SecondViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .blue
+        let label_2 = UILabel(frame: view.frame)
+        label_2.text = "Screen 2"
+        view.addSubview(label_2)
+
+        // set Button by func
+        transScreenButton()
     }
 
+    // make button
+    func transScreenButton()  {
+        let button_2 = UIButton(frame: CGRect(x: self.view.frame.width / 2 - 200 / 2, y: self.view.frame.height / 2 - 150 / 2, width: 200, height: 150))
+        button_2.titleLabel?.text = "Screen Transition Button"
+        button_2.backgroundColor = .orange
+        button_2.addTarget(self, action: #selector(transScreen), for: .touchUpInside)
+
+        view.addSubview(button_2)
+
+    }
+
+
+    // set Screen Transaction Action
+
+    @objc func transScreen() {
+        let secondScreen = FirstViewController()
+
+        secondScreen.modalTransitionStyle = .coverVertical
+        present(secondScreen, animated: true, completion: nil)
+
+    }
 
 }
 
