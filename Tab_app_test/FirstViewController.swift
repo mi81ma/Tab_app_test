d1de3e4aba320f176bf21f49acfd73ee6e1bf7d8
masato <masato@m-mbp.local>
2018-11-06 13:48:33 +0800

    Initial Commit

---
+++ Tab_app_test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tab_app_test
+//
+//  Created by masato on 6/11/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tab_app_test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tab_app_test
+//
+//  Created by masato on 6/11/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
